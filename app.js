43e5c54c0e14c293ab4e66ece21f3ebc761491bd
JeremyL95 <jeremythegreat95@gmail.com>
2024-12-27 13:49:49 +0800

    build components :q

--- app.js
+++ app.js
@@ -1,4 +1,101 @@
+import { advantages, progressBar, careerPackages } from './public/data/data.js';
 
+let advantageContainer = ``;
+let progressBarContainer = ``;
+let careerPackagesContainer = ``;
 
+// advantages card
+advantages.forEach((advantage) => {
+    advantageContainer += `
+        <div id="${advantage.id}" class="advantage">
+            <img src='${advantage.icon}' alt="icon"
+                class="advantage-icon">
+
+            <h3 class="advantage-title">
+                ${advantage.title}
+            </h3>
+
+            <p class="advantage-description">
+                ${advantage.description}
+            </p>
+        </div>
+        
+    `
+});
+document.getElementById('advantages').innerHTML = advantageContainer;
+
+// progress bar
+progressBar.forEach((progressBar) => {
+    progressBarContainer += `
+        <div id="progressBar${progressBar.id}" class="progress-bar-container">
+            <div class="progress-bar-icon" style="width: 20%">
+                i
+            </div>
+
+            <div class="" style="width: 80%">
+                <div class="progress-bar-top">
+                    <h3 class="progress-bar-title">
+                        ${progressBar.title}
+                    </h3>
+
+                    <div class="progress-bar-percentage">
+                        ${progressBar.filled}%
+                    </div>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress-bar-inner" style="width: ${progressBar.filled}%"></div>
+                </div>
+            </div>
+        </div>`
+});
+document.getElementById('progressBarContainer').innerHTML = progressBarContainer;
+
+// career packages
+careerPackages.forEach((careerPackage) => {
+    careerPackagesContainer += `<div class="career-card col-12 col-md-4 my-2 ${careerPackage.recommended ? 'recommended' : ''}">
+                <div class="recommend-package">
+                
+                </div>
+
+                <h3 class="text-center">
+                    ${careerPackage.title}
+                </h3>
+
+                <h2 class="text-center">
+                    ${careerPackage.price} Riyal / day
+                </h2>
+
+                <div class="career-image">
+                    <img src="${careerPackage.image}" alt="careers">
+                </div>
+
+                <p>${careerPackage.description}</p>
+
+                <hr>
+                
+                <ul>`
+
+    careerPackage.lists.map((list, index) => {
+        careerPackagesContainer += `
+        <li id="list${index + 1}" class="my-2">
+            ${list}
+            
+             <div>
+                 <i class="fa-solid fa-check"></i>
+             </div>
+         </li>`
+    })
+
+    careerPackagesContainer += `</ul>
+                <div class="d-flex justify-content-center">
+                    <button class="btn-apply my-3">
+                        Apply Now
+                    </button>
+                </div>
+            </div>`
+});
+document.getElementById('careerCards').innerHTML = careerPackagesContainer;
+
+// footer
 const currentYear = new Date().getFullYear();
 document.getElementById('copyright').innerText = `Copyright © ${currentYear} Talents Weave. All Rights Reserved.`
---
+++ public/data/data.js
@@ -0,0 +1,109 @@
+export const advantages = [
+    {
+        id: 1,
+        icon: 'https://talentsweave.com/wp-content/uploads/2024/09/clock-eleven-thirty.svg',
+        title: 'Flexible Working Hours',
+        description: 'Enjoy the freedom to create a work schedule that fits your life. Whether you\'re an early riser or a night owl, Talents Weave connects you with jobs that offer the flexibility to work when you\'re most productive.'
+    },
+    {
+        id: 2,
+        icon: './public/assets/balance.webp',
+        title: 'High-Pay Salary',
+        description: 'We partner with companies that value your skills and expertise, offering competitive salaries that reflect your worth. Find roles that not only match your qualifications but also provide the financial rewards you deserve.'
+    },
+    {
+        id: 3,
+        icon: './public/assets/money.webp',
+        title: 'Work-Life Balance',
+        description: 'Say goodbye to long commutes and rigid office hours. Our remote opportunities allow you to maintain a healthy work-life balance, giving you more time to spend with family, pursue hobbies, or simply relax.'
+    },
+    {
+        id: 4,
+        icon: './public/assets/cost.webp',
+        title: 'Cost Savings',
+        description: 'Working remotely means more savings on daily expenses like commuting, meals, and work attire. Talents Weave helps you maximize your earnings by connecting you with roles that eliminate unnecessary costs.'
+    },
+    {
+        id: 5,
+        icon: './public/assets/profit.webp',
+        title: 'Personal Growth Opportunities',
+        description: 'Remote work opens doors to global opportunities, allowing you to collaborate with diverse teams and develop new skills. At Talents Weave, we’re committed to helping you grow both professionally and personally, paving the way for a fulfilling career.'
+    },
+]
+
+export const progressBar = [
+    {
+        id: 1,
+        title: 'Fully Work From Home',
+        icon: '',
+        filled: 100,
+        max_fill: 100
+    },
+    {
+        id: 2,
+        title: 'Worldwide Colleagues',
+        icon: '',
+        filled: 85,
+        max_fill: 100
+    },
+
+    {
+        id: 3,
+        title: 'Growth Opportunities',
+        icon: '',
+        filled: 93,
+        max_fill: 100
+    }
+]
+
+export const careerPackages = [
+    {
+        id: 1,
+        title: 'App Promoter',
+        price: 600,
+        image: './public/assets/career-1.webp',
+        description: 'We are looking for an enthusiastic App Promoter to help boost the visibility and downloads of our mobile application. Your role will involve engaging with potential users on social media, spreading the word about our app, and encouraging people to try it out.',
+        lists: [
+            'No Experience Required',
+            '21 Years Old and Above',
+            'Work Remotely From Home',
+            'Compatible with Smartphone or Laptop',
+            'Part Time or Full Time Available',
+            'Online Training Provided'
+        ],
+        recommended: 0
+    },
+
+    {
+        id: 2,
+        title: 'E-commerce Online Assistant',
+        price: 625,
+        image: './public/assets/career-2.webp',
+        description: 'Join our team as an eCommerce Online Assistant, where you\'ll assist with managing product listings, processing orders, and providing basic customer support for our online store. This role is perfect for someone who is organized and eager to learn about the world of eCommerce.',
+        lists: [
+            'No Experience Required',
+            '21 Years Old and Above',
+            'Work Remotely From Home',
+            'Compatible with Smartphone or Laptop',
+            'Part Time or Full Time Available',
+            'Online Training Provided'
+        ],
+        recommended: 1
+    },
+    {
+        id: 3,
+        title: 'Virtual Assistant',
+        price: 610,
+        image: './public/assets/career-3.webp',
+        description: 'We are seeking a dependable Virtual Assistant to help with a variety of administrative tasks such as managing emails, scheduling appointments, and handling data entry. This position is ideal for someone who is organized, proactive, and ready to start working from home.',
+        lists: [
+            'No Experience Required',
+            '21 Years Old and Above',
+            'Work Remotely From Home',
+            'Compatible with Smartphone or Laptop',
+            'Part Time or Full Time Available',
+            'Online Training Provided'
+        ],
+        recommended: 0
+    },
+]
